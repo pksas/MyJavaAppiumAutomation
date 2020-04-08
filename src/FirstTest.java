476268b72895071796de5770aa40754a7feff7e4
pksas <plsas82@gmail.com>
2020-04-08 20:03:10 +0300

    Optimized solution of Ex4
    Бесконечный цикл заменен разовым получением массива элементов поиска и перебором по всем элементам.

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -6,13 +6,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.util.List;
 
 public class FirstTest {
 
@@ -235,33 +235,18 @@ public class FirstTest {
                 5
         );
 
-        //попытка определить количество резульатов поиска на странице
-        WebElement last_element = waitForElementPresent(
-                By.xpath("//*[@resource-id='org.wikipedia:id/search_results_list']/*[last()]"),
+        waitForElementPresent(
+                By.xpath("//*[@resource-id='org.wikipedia:id/search_results_list']/*[1]"),
                 "This search has no result!",
-                5
+                15
         );
-        System.out.println("The index of last element is: " + last_element.getAttribute("index"));
 
-        int container_number = 1;
-        int timeout_in_seconds = 15;
-        while (true)
-        {
-            try {
-                WebElement element = waitForElementPresent(
-                        By.xpath("//*[@resource-id='org.wikipedia:id/search_results_list']/*[" + container_number + "]//*[@resource-id='org.wikipedia:id/page_list_item_title']"),
-                        "Nobody will read this message)))",
-                        timeout_in_seconds
-                );
-                boolean isSearchWordPresence = element.getAttribute("text").toLowerCase().contains(search_word.toLowerCase());
-                Assert.assertTrue("The searched word is not in the title of the article  " + container_number, isSearchWordPresence);
-                timeout_in_seconds = 1; // Если мы тут, то все результаты поиска уже на экране и нет необходимости в большом таймауте.
-                container_number++;
-            } catch (TimeoutException e) {
-                break;
-            }
+        List<WebElement> search_title_elements = driver.findElements(By.id("org.wikipedia:id/page_list_item_title"));
+
+        for (WebElement element : search_title_elements) {
+            boolean isSearchWordPresence = element.getAttribute("text").toLowerCase().contains(search_word.toLowerCase());
+            Assert.assertTrue("The searched word is not in all titles of articles!!!", isSearchWordPresence);
         }
-        Assert.assertTrue("This search has no result!", container_number != 1);
     }
 
     private void checkingDefaultTextInSearchPlate() {

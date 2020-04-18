5c5e79fbb6abaf76299d69401308e9de88fad383
pksas <plsas82@gmail.com>
2020-04-18 10:55:20 +0300

    Transfer setUp and tearDown logic to CoreTestCaseClass
    Перенос логики запуска тестов в отдельный класс.

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,49 +1,17 @@
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.TouchAction;
-import io.appium.java_client.android.AndroidDriver;
-import org.junit.After;
+import lib.CoreTestCase;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.net.URL;
 import java.util.List;
 
-public class FirstTest {
-
-    private AppiumDriver driver;
-
-    @Before
-    public void setUp() throws Exception
-    {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "AndroidTestDrive");
-        capabilities.setCapability("platformVersion", "8.0");
-        capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("appPackage", "org.wikipedia");
-        capabilities.setCapability("appActivity", ".main.MainActivity");
-        capabilities.setCapability("app", "C:/Users/User/Desktop/MyJavaAppiumAutomation/apks/org.wikipedia.apk");
-
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-    }
-
-    @After
-    public void tearDown()
-    {
-        if (driver.getOrientation().equals(ScreenOrientation.LANDSCAPE)) {
-            driver.rotate(ScreenOrientation.PORTRAIT);
-        }
-        driver.quit();
-    }
+public class FirstTest extends CoreTestCase {
 
     @Test
-    public void simpleCrushTest()
+    public void testSimpleCrush()
     {
         WebElement test_element = waitForElementPresent(
                 By.id("org.wikipedia:id/view_featured_article_card_article_title"),
@@ -78,7 +46,7 @@ public class FirstTest {
 }
 
     @Test
-    public void firstTest()
+    public void testSearch()
     {
         waitForElementAndClick(
                 By.xpath("//*[contains(@text,'Search Wikipedia')]"),
